e06c03948819162b603dbb2e6ae5cb364f9c017c
Isauro Manuel Rodriguez <90867@sistemas.frc.utn.edu.ar>
2023-11-10 18:56:29 -0300

    seguridad aplicada

--- ApiGW/src/main/java/com/example/gateway/GWConfig.java
+++ ApiGW/src/main/java/com/example/gateway/GWConfig.java
@@ -1,21 +1,57 @@
 package com.example.gateway;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+//import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+//import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 @Configuration
-//@EnableWebFluxSecurity
 public class GWConfig {
 
     @Bean
     public RouteLocator configurarRutas(RouteLocatorBuilder builder) {
         return builder.routes()
-                // Ruteo al Microservicio de Personas
-                .route(p -> p.path("/alquileres").uri("http://localhost:8080")) //tengo la opcion de utilizar .filters(f -> rewritePath("/servicio-alumnos/", "/alumnos/") //todo esto se hace antes del .uri y permite reescribir la ruta
-                                                                                                                //.setRequestHeader("pepe" , "123456contra") ) //request header me permite agregar un header, segun un valor puntual que me haga falta, para que el servicio que esta detras lo reconozca y manejar por ejemplo los roles enviando el header con el rol que necesito
+                // Ruteo al Microservicio de estaciones y alquileres
+                .route(p -> p.path("/estaciones").uri("http://localhost:8081"))
+                .route(p -> p.path("/alquileres").uri("http://localhost:8082"))
                 .build();
 
     }
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(exchanges -> exchanges
+                                .pathMatchers("/api/estaciones")
+                                .hasAnyRole("CLIENTES", "ADMINISTRADORES")
+                                .anyExchange()
+                                .authenticated()
+                ).oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()))
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        return jwtAuthenticationConverter;
+    }
+
 }
